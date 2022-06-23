d16780a3f10b51e23dc594e7140fcb434d4ff072
Aleksandr_Seleznev <SeleznevAIu86@gmail.com>
2022-06-23 14:32:15 +0300

    Changed some features

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import telebot
 
-from config import TOKEN, available_currency
+from config import *
 from extensions import GottenCurrency, APIException
 bot = telebot.TeleBot(TOKEN)
 
@@ -11,12 +11,12 @@ def handle_start_help(message: telebot.types.Message):
  <имя валюты, цену которой вы хотите узнать> \
 <имя валюты, в которой надо узнать цену первой валюты> \
 <количество первой валюты>\n\
-При вводе команты /value выводится информация о всех доступных валютах'
+При вводе команды /value выводится информация о всех доступных валютах'
     bot.reply_to(message, text)
 
 
 @bot.message_handler(commands=['value'])
-def handle_start_help(message: telebot.types.Message):
+def handle_value(message: telebot.types.Message):
     text = 'Доступныe для конвертации валюты:'
     for i in available_currency:
         text = text + f'\n- {str(i)}'
@@ -24,14 +24,14 @@ def handle_start_help(message: telebot.types.Message):
 
 
 @bot.message_handler(content_types=['text'])
-def handle_start_help(message):
+def convert_currency(message):
     try:
-        gotten_data = message.text.lower().split(" ")
-        if len(gotten_data) != 3 :
+        gotten_data = message.text.lower().split()
+        if len(gotten_data) != 3:
             raise APIException("Неверное количество переменных!")
         base, quote, amount = gotten_data
-        request_currency = GottenCurrency.get_price(base, quote, amount)
-        text = f'Результат конверсии: {request_currency}'
+        request_currency, from_, to_, amount = GottenCurrency.get_price(base, quote, amount)
+        text = f'Результат конверсии: {amount} {from_} = {request_currency} {to_}'
     except APIException as e:
         bot.send_message(message.chat.id, f'Неправильно введены данные:\n{e}')
     except Exception as e:
@@ -41,4 +41,3 @@ def handle_start_help(message):
 
 
 bot.polling()
-
--- extensions.py
+++ extensions.py
@@ -10,14 +10,37 @@ class APIException(Exception):
 class GottenCurrency:
     @staticmethod
     def get_price(base, quote, amount):
+        amount = amount.replace(',', '.')
         if not available_currency.get(base):
             raise APIException(f'Неправильное название валюты "{base}" или ее нет в списке!')
         elif not available_currency.get(quote):
             raise APIException(f'Неправильное название валюты "{quote}" или ее нет в списке!')
         elif base == quote:
             raise APIException("Одинаковые валюты!")
-        if not amount.isdigit() or int(amount) <= 0:
+        try:
+            if amount.isdigit():
+                amount = int(amount)
+            else:
+                amount = float(amount)
+            if amount <= 0:
+                raise APIException("Количество первой валюты!")
+        except Exception:
             raise APIException("Количество первой валюты!")
-        request_currency = requests.get(f'https://api.apilayer.com/fixer/convert?to={available_currency[quote]}\
-&from={available_currency[base]}&amount={amount}', {"apikey": 'wabT2uTsxApcWjQdCN1ukNf1vGupRhUz'}).content
-        return json.loads(request_currency)["result"]
+        request_currency = requests.get(f'https://api.apilayer.com/fixer/convert?to={available_currency[quote][0]}\
+&from={available_currency[base][0]}&amount={amount}', {"apikey": 'wabT2uTsxApcWjQdCN1ukNf1vGupRhUz'}).content
+        result_conversion = json.loads(request_currency)["result"]
+        result = round(float(result_conversion), 2)
+        if amount < 1:
+            from_ = available_currency[base][1]
+        elif amount > 1:
+            from_ = available_currency[base][2]
+        else:
+            from_ = base
+        if result < 1:
+            to_ = available_currency[quote][1]
+        elif result > 1:
+            to_ = available_currency[quote][2]
+        else:
+            to_ = quote
+        return result, from_, to_, amount
+
